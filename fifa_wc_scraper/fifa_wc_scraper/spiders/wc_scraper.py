51c45b10708c27b4e6843efe0ace232ad314aad1
Karthik Subramanian <karthik.s.sundaram@gmail.com>
2014-04-26 18:43:47 +0530

    WC Scraper phase 2

--- fifa_wc_scraper/fifa_wc_scraper/spiders/wc_scraper.py
+++ fifa_wc_scraper/fifa_wc_scraper/spiders/wc_scraper.py
@@ -11,9 +11,22 @@ class wc_scraper(Spider):
   allowed_domains = ['en.wikipedia.org']
   start_urls = ['http://en.wikipedia.org/wiki/FIFA_World_Cup']
   
+  def enc(st):
+    return st.encode('ascii','ignore')
+
   def parse(self, response):
     extractor = SgmlLinkExtractor(allow =('wiki/[0-9]{4}\_FIFA\_World\_Cup$'))
     links = extractor.extract_links(response)
     for i in links:
-      print i
-    print 'over'
+      if int(i.text[:4]) < 2014:
+        yield Request(i.url, callback=self.parse_items)  
+    print 'over machi'
+
+  def parse_items(self, response):
+    sel = Selector(response)
+    matches = sel.xpath('//*[@id="mw-content-text"]/div[contains(@class, "vevent")]').extract()
+    for m in matches:
+      m_date = enc(m[0].xpath('./table[1]/tr/td/div/text()').extract()[0])
+      m_time = enc(m[0].xpath('./table[1]/tr/td/div/a/text()').extract()[0])
+
+
